edf058e03ec7475cfdbbc479d8dc6f339ed638a1
answershuto <905307789@qq.com>
2016-08-10 22:34:23 +0800

    增加删除界面

--- views/js/components/title.react.js
+++ views/js/components/title.react.js
@@ -18,18 +18,22 @@ var NoteShow = React.createClass({
 
 	render: function(){
 		return <div className={this.props.bgClass}>
-			<div>
-				<span>时间：</span>
-				<span>{this.props.date}</span>
+			<div className="ui-width-900 ui-float-left">
+				<div>
+					<span>时间：</span>
+					<span>{this.props.date}</span>
+				</div>
+				<div>
+					<span>标题：</span>
+					<span>{this.props.title}</span>
+				</div>
+				<div>
+					<span>内容：</span>
+					<span>{this.props.text}</span>
+				</div>
 			</div>
-			<div>
-				<span>标题：</span>
-				<span>{this.props.title}</span>
-			</div>
-			<div>
-				<span>内容：</span>
-				<span>{this.props.text}</span>
-			</div>
+			<div className="ui-del-div ui-float-right glyphicon glyphicon-remove"></div>
+			<div className="ui-clear-both"></div>
 		</div>
 	}
 })

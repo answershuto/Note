c45627e448dff08d67f5bad1218edf310fd2f403
answershuto <905307789@qq.com>
2016-08-28 15:37:21 +0800

    修改头像上传界面

--- views/js/components/edit.react.js
+++ views/js/components/edit.react.js
@@ -7,7 +7,9 @@ var Edit = React.createClass({
 	
 	getInitialState: function(){
 		return {
-			editType: this.props.editType
+			uploadFile: "",
+			editType: this.props.editType,
+			imageSrc: this.props.imageSrc || "../../image/defaultHeadPortrait.png"
 		};
 	},
 
@@ -24,12 +26,21 @@ var Edit = React.createClass({
 		var show = "";
 		switch(this.state.editType){
 			case 'userImage':/*头像*/
-				show = <div>
-					<form action="/note/uploadIcon" method="post" encType="multipart/form-data" target="userImage">
-						<input type="file" accept="image/*" name="image"></input>
-						<input type="submit" value="上传头像"></input>
+				show = <div className="">
+					<div className="ui-edit-icon-div">
+						<img onClick={this.handleIconClick} className="ui-edit-icon img-circle" src={this.state.imageSrc}></img>
+					</div>
+					<div className="ui-edit-notes-div">
+						<span>注：头像大小不得超过2M</span>
+					</div>
+					<div className="ui-edit-upload-div">
+						<button onClick={this.handleIconUploadClick} className="btn btn-lg btn-default btn-block">上传</button>
+					</div>
+					<form className="ui-display-none" action="/note/uploadIcon" method="post" encType="multipart/form-data" target="userImage">
+						<input id="edit_file" onChange={this.handleFileChange} type="file" accept="image/*" name="image"></input>
+						<input id="edit_upload" type="submit" value="上传头像"></input>
 					</form>
-					<iframe name="userImage"></iframe>
+					<iframe name="userImage" className="ui-display-none"></iframe>
 				</div>
 				break;
 			case 'nikeName':/*用户名*/
@@ -41,7 +52,26 @@ var Edit = React.createClass({
 		return <div className="">
 			{show}
 		</div>
+	},
+
+	handleIconClick: function(e){
+		$('#edit_file').click();
+	},  
+
+	handleFileChange: function(e){
+		this.state.uploadFile = e.target.value;
+	},
+
+	handleIconUploadClick: function(e){
+		if (this.state.uploadFile) {
+			$('#edit_upload').click();
+		}
+		else{
+			alert('请选择上传的图片');
+		}
 	}
+
+
 })
 
 module.exports = Edit;

3347e5f007fe81de4072d7a82b216c3e22363b90
answershuto <905307789@qq.com>
2016-09-01 23:08:49 +0800

    修改关于我们界面

--- views/js/components/aboutUs.react.js
+++ views/js/components/aboutUs.react.js
@@ -27,10 +27,21 @@ var AboutUs = React.createClass({
 			<div className="ui-about-name">
 				<span>染陌云笔记 1.3.2</span>
 			</div>
-			<div className="ui-about-information">
-				<div>作者： 曹阳</div>
-				<div>备注： 本软件只供学习使用，不做任何商业用途。</div>
+			<div>
+				<div className="col-md-1 col-xs-1"></div>
+				<div className="col-md-10 col-xs-10 ui-about-center">
+					<div>作者： 曹阳</div>
+					<div>备注： 本软件只供学习使用，不做任何商业用途。</div>
+					<div>
+						Github:<a href="https://github.com/answershuto">https://github.com/answershuto</a>
+					</div>
+					<div>
+						邮箱：<a href="answershuto@126.com">answershuto@126.com</a>
+					</div>
+				</div>
+				<div className="col-md-1 col-xs-1"></div>
 			</div>
+			<div className="fixed">©2016 CaoYang , All rights reserved.</div>
 		</div>
 	}
 })
